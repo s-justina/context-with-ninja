4c3453775e688a0333b37b5e435bee021a260b7d
s-justina <jus.sobcz@gmail.com>
2021-03-04 22:15:36 +0100

    usage ThemeContext in existing components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,22 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-    render(){
-        return(
-            <nav>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        )
-    }
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{background: theme.ui, color: theme.syntax}}>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
